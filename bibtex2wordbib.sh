0ba44a63290980bb25c2c0da10f28c6f3e14dafa
msdm <8253237+msdm@users.noreply.github.com>
2020-11-01 11:55:50 +0330

    Update bibtex2wordbib.sh

--- bibtex2wordbib.sh
+++ bibtex2wordbib.sh
@@ -37,15 +37,15 @@
 # newline (,\n). So, make sure your .bib file is formatted properly
 # (one field per line)
 #
-# This code is written based on ECMA-376 standard
+# This code has been written based on ECMA-376 standard
 # (https://www.ecma-international.org/publications/standards/Ecma-376.htm),
 # which defines Microsoft Office XML file format including the bibliography XML.
-# If language field is not set for an item, it will be set to English by 
+# If the language field is not set for an item, it will be set to English by 
 # default.
 #
 # Usage: bibtex2wordbib.sh <bibtex file> [<output file>]
 #
-# If output file is not provided, the generated Bibliography XML will be
+# If the output file is not provided, the generated Bibliography XML will be
 # printed in the stdout. Otherwise, with writing the generated XML in the file,
 # conversion report will be printed in the stdout.
 
